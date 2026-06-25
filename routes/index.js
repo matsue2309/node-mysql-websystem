4b148bb086c4d60a688b931e8f5de3364e5cb21f
matsue2309 <j2309@matsue-ct.ac.jp>
2026-06-25 07:15:27 +0000

    [wip]サインアップ機能を作成しよう

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,40 @@
 const express = require('express');
 const router = express.Router();
-
-let todos = [];
+const knex = require('../db/knex');
 
 router.get('/', function (req, res, next) {
-  res.render('index', {
-    title: 'ToDo App',
-    todos: todos,
-  });
+  knex("tasks")
+    .select("*")
+    .then(function (results) {
+      console.log(results);
+      res.render('index', {
+        title: 'ToDo App',
+        todos: results,
+      });
+    })
+    .catch(function (err) {
+      console.error(err);
+      res.render('index', {
+        title: 'ToDo App',
+      });
+    });
 });
 
 router.post('/', function (req, res, next) {
   const todo = req.body.add;
-  todos.push(todo);
-  res.redirect('/');
+  knex("tasks")
+    .insert({ user_id: 1, content: todo })
+    .then(function () {
+      res.redirect('/');
+    })
+    .catch(function (err) {
+      console.error(err);
+      res.render('index', {
+        title: 'ToDo App',
+      });
+    });
 });
+
+router.use('/signup', require('./signup'));
 
 module.exports = router;
