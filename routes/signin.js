658a404a736e04d34d0cedb51b1dcaf470d33ef5
matsue2309 <j2309@matsue-ct.ac.jp>
2026-07-07 08:47:03 +0000

    パスワードの照合をハッシュ化に対応させよう

--- routes/signin.js
+++ routes/signin.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const knex = require("../db/knex");
+const bcrypt = require("bcrypt");
 
 router.get('/', function (req, res, next) {
   const userId = req.session.userid;
@@ -19,22 +20,27 @@ const isAuth = Boolean(userId);
 
   knex("users")
     .where({
-      name: username,
-      password: password,
-    })
+  name: username,
+})
     .select("*")
-    .then((results) => {
-      if (results.length === 0) {
-        res.render("signin", {
-          isAuth: isAuth,
-          title: "Sign in",
-          errorMessage: ["ユーザが見つかりません"],
-        });
-      } else {
-        req.session.userid = results[0].id;
-        res.redirect('/');
-      }
-    })
+    .then(async function (results) {
+  if (results.length === 0) {
+    res.render("signin", {
+      title: "Sign in",
+      errorMessage: ["ユーザが見つかりません"],
+      isAuth: isAuth,
+    });
+  } else if (await bcrypt.compare(password, results[0].password)) {
+    req.session.userid = results[0].id;
+    res.redirect('/');
+  } else {
+    res.render("signin", {
+      title: "Sign in",
+      errorMessage: ["ユーザが見つかりません"],
+      isAuth: isAuth,
+    });
+  }
+})
     .catch(function (err) {
       console.error(err);
       res.render("signin", {
