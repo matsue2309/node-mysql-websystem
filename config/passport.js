e472377b7a9478f2a7fb75a1078cd8a4dd9b7e9d
matsue2309 <j2309@matsue-ct.ac.jp>
2026-07-09 06:33:44 +0000

    passport認証の動作確認をしよう

--- config/passport.js
+++ config/passport.js
@@ -8,18 +8,19 @@ const secret = "secretCuisine123";
 
 module.exports = function (app) {
   passport.serializeUser(function (user, done) {
+    console.log("serializeUser");
     done(null, user.id);
   });
 
-  passport.deserializeUser(async function (id, done) {
-    try {
-      const user = await User.findById(id);
-      done(null, user);
-    } catch (error) {
-      done(error, null);
-    }
-  });
-
+passport.deserializeUser(async function (id, done) {
+ console.log("deserializeUser");
+ try {
+ const user = await User.findById(id);
+ done(null, user);
+ } catch (error) {
+ done(error, null);
+ }
+});
   passport.use(new LocalStrategy({
       usernameField: "username",
       passwordField: "password",
@@ -55,5 +56,6 @@ module.exports = function (app) {
     })
   );
 
+  app.use(passport.initialize());
   app.use(passport.session());
 };
